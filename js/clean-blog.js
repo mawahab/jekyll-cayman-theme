649f28420cee9fc8aa659fd99e1c3b3165d82f81
mawahab <muhammadabdul.wahab@centralesupelec.fr>
2017-07-24 15:15:39 +0200

    Update clean-blog.js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -13,34 +13,21 @@ $(function() {
 $(function() {
     $("#contactForm").submit(function(e) {
       e.preventDefault();
-    var $submit = $('input:submit', $contactForm);
-	var defaultSubmitText = $submit.val();
-
-	$.ajax({
-		url: '//formspree.io/abdulparis@gmail.com',
+      	$.ajax({
+		url: '//formspree.io/your@email.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
 		beforeSend: function() {
-			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-			$submit.attr('disabled', true).val('Sending message…');
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
 		},
 		success: function(data) {
-			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-			$submit.val('Message sent!');
-			setTimeout(function() {
-				//$('.alert--success').remove();
-				$submit.attr('disabled', false).val(defaultSubmitText);
-			}, 5000);
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
 		},
 		error: function(err) {
-			//$contactForm.find('.alert--loading').hide();
-			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-			$submit.val('Ops, there was an error.');
-			setTimeout(function() {
-				//$('.alert--error').remove();
-				$submit.attr('disabled', false).val(defaultSubmitText);
-			}, 5000);
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
 		}
 	});
 });
